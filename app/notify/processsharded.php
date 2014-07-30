798ea977bff62cb30530eddf7fdd30a31e186f1f
nesan <nesan@vembu.com>
2014-07-30 17:43:37 +0530

    Bulk emailing thread modified to send 150 emails per thread to avoid too many simultaneous php.exe processes. 150 emails could be sent in 30 mins approx.

--- app/notify/processsharded.php
+++ app/notify/processsharded.php
@@ -12,6 +12,7 @@
 
 	$shardedDB = base64_decode($_GET["shardedDB"]);//Exclusively for running from command line 
 	$events_obj = new Events($APPLICATION_PATH, $shardedDB);
+	$events_list = array();
 	//$events_list = $events_obj->listEventDetailsToNotifyNow();
 
 	/** /
@@ -19,7 +20,13 @@
 	$events_list[1] = array(0=>array(0=>array("1@jujukin.com","2@jujukin.com","3@jujukin.com","4@jujukin.com","5@jujukin.com","6@jujukin.com","7@jujukin.com","8@jujukin.com","9@jujukin.com","10@jujukin.com","11@jujukin.com","12@jujukin.com","13@jujukin.com","14@jujukin.com","15@jujukin.com","16@jujukin.com"), 1=>"this is the subject", 2=>"this is th ebody"), 1=>array(0=>array("nesanjoseph@gmail.com"),  1=>"this is oho subject", 2=>"this is oho ebody"));
 	/**/
 
-	$email_sending_file = $APPLICATION_PATH."notify/sendemail.php";;
+	if(!is_array($events_list) || (is_array($events_list) && COUNT($events_list) < 2) || $events_list[0]==0 || COUNT($events_list[1]) <= 0) {
+		exit;
+	}
+
+	$email_sending_file = $APPLICATION_PATH."notify/sendemail.php";
+
+
 	if($events_list[0]==1)
 	{
 		$commands = array();
@@ -32,22 +39,20 @@
 			$comma_separated_email_list = "";
 			for($k=0; $k < $emails_count; $k++)
 			{
-				$curr_email = $events_list[1][$e][0][$k];
-				$comma_separated_email_list .= ((trim($comma_separated_email_list)=="")? $curr_email : ",".$curr_email); 
-				if($emails_count > 25)
+				$tmp = 0;
+				$comma_separated_email_list = "";
+				while($tmp < 150 && $k < $emails_count)
 				{
-					if(($k%25==0 && $k > 0) || $k+25 > $emails_count)
-					{
-						$commands[] = '"C:/Program Files (x86)/php/php.exe" '.$email_sending_file.' csvToEmails='.base64_encode($comma_separated_email_list).' subject='.base64_encode($subject).' emailBody='.base64_encode($body).' fromAddressType='.$fromAddressType;
-						$comma_separated_email_list = "";
-					}
+					$curr_email = $events_list[1][$e][0][$k];
+					$comma_separated_email_list .= ((trim($comma_separated_email_list)=="")? $curr_email : ",".$curr_email); 
+					$tmp++;
+					$k++;
 				}
-				else
-				{
-					$commands[] = '"C:/Program Files (x86)/php/php.exe" '.$email_sending_file.' csvToEmails='.base64_encode($comma_separated_email_list).' subject='.base64_encode($subject).' emailBody='.base64_encode($body).' fromAddressType='.$fromAddressType;
-					$comma_separated_email_list = "";
-				}
+				$k--;//To adjust the above extra increment...
+
+				$commands[] = '"C:/Program Files (x86)/php/php.exe" '.$email_sending_file.' csvToEmails='.base64_encode($comma_separated_email_list).' subject='.base64_encode($subject).' emailBody='.base64_encode($body).' fromAddressType='.$fromAddressType;
 			}
+
 		}
 
 		$threads = new Multithread( $commands );
@@ -56,7 +61,7 @@
 		foreach ( $threads->commands as $key=>$command )
 		{
 			//echo "Command: ".$command."\n\n";
-			//echo "Output: ".$threads->output[$key];
+			echo "Sharded Output: ".$threads->output[$key]."\n";
 			//echo "Error: ".$threads->error[$key]."\n\n";
 		}
 		/**/
